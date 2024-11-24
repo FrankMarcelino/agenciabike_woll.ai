10802de098ef97427e4219782895f234a82a6fc6
frankMarcelino <frank.silva.ti@gmail.com>
2024-11-23 23:56:21 -0300

    fui criativo kkkk

--- js/app.js
+++ js/app.js
@@ -175,34 +175,65 @@ document.querySelectorAll(".faq-item").forEach((item) => {
 });
 
 // hero carrousel
+// document.addEventListener("DOMContentLoaded", () => {
+//   const buttons = document.querySelectorAll(".carousel-nav");
+//   const headers = {
+//     "header-1": document.querySelector(".header-1"),
+//     "header-2": document.querySelector(".header-2"),
+//   };
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//       const targetClass = button.getAttribute("data-target");
+//       const activeButton = document.querySelector(".carousel-nav.active");
+//       const activeHeader = document.querySelector(".container-header.active");
+//       if (activeButton !== button) {
+//         activeButton.classList.remove("active");
+//         button.classList.add("active");
+//         activeHeader.classList.remove("active");
+//         activeHeader.classList.add("exit");
+//         headers[targetClass].classList.add("active");
+//         setTimeout(() => {
+//           activeHeader.classList.remove("exit");
+//         }, 500);
+//         // Tempo deve coincidir com a duração da transição
+//         // Atualiza o indicador
+//         const indicator = document.querySelector(".carousel-indicator");
+//         const slideIndex = Array.from(buttons).indexOf(button) + 1;
+//         indicator.textContent = `Slide ${slideIndex} de ${buttons.length}`;
+//       }
+//     });
+//   });
+//   // Inicializa o primeiro botão e indicador como ativo
+//   buttons[0].classList.add("active");
+//   const indicator = document.querySelector(".carousel-indicator");
+//   indicator.textContent = `Slide 1 de ${buttons.length}`;
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".btn--solucoes");
+  const buttons = document.querySelectorAll(".carousel-nav");
   const headers = {
     "header-1": document.querySelector(".header-1"),
     "header-2": document.querySelector(".header-2"),
   };
-
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       const targetClass = button.getAttribute("data-target");
-      const activeButton = document.querySelector(".btn--solucoes.active");
-      const activeSection = document.querySelector(
-        ".section-cards-headers > div.active"
-      );
-
+      const activeButton = document.querySelector(".carousel-nav.active");
+      const activeHeader = document.querySelector(".container-header.active");
       if (activeButton !== button) {
         activeButton.classList.remove("active");
         button.classList.add("active");
-
-        activeSection.classList.remove("active");
-        activeSection.classList.add("exit");
-
+        activeHeader.classList.remove("active");
         headers[targetClass].classList.add("active");
-
-        setTimeout(() => {
-          activeSection.classList.remove("exit");
-        }, 500); // Tempo deve coincidir com a duração da transição
+        // Atualiza o indicador
+        const indicator = document.querySelector(".carousel-indicator");
+        const slideIndex = Array.from(buttons).indexOf(button) + 1;
+        indicator.textContent = `Slide ${slideIndex} de ${buttons.length}`;
       }
     });
   });
+  // Inicializa o primeiro botão e indicador como ativo
+  buttons[0].classList.add("active");
+  const indicator = document.querySelector(".carousel-indicator");
+  indicator.textContent = `Slide 1 de ${buttons.length}`;
 });
