93b232e4f0c038ed75653bf4e0e682fdd7feff49
frankMarcelino <frank.silva.ti@gmail.com>
2024-11-25 08:20:39 -0300

    animação das emagens da empresa iniciada

--- js/app.js
+++ js/app.js
@@ -213,3 +213,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   updateCarousel(0); // Initialize the first slide as active
 });
+
+// animacao das imagens da empres
+
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".img-empresa");
+  let currentIndex = 0;
+
+  function showImage(index) {
+    images.forEach((img, i) => {
+      img.classList.toggle("active", i === index);
+    });
+  }
+
+  function startImageLoop() {
+    setInterval(() => {
+      showImage(currentIndex);
+      currentIndex = (currentIndex + 1) % images.length; // Incrementa o índice e reseta ao atingir o total de imagens
+    }, 1500); // Troca a cada 1 segundo
+  }
+
+  startImageLoop(); // Inicia o loop
+});
