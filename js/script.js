159c08ae1cb998c985d8ebf5af09cd7b28e56edd
frankMarcelino <frank.silva.ti@gmail.com>
2024-11-29 10:03:12 -0300

    imapcto

--- js/script.js
+++ js/script.js
@@ -164,3 +164,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
   startImageLoop(); // Inicia o loop
 });
+
+//animacao impacto
+
+document.addEventListener("DOMContentLoaded", () => {
+  const letters = document.querySelectorAll(".impacto-word .letter");
+  const words = document.querySelectorAll(".impacto-list .word");
+  let index = 0;
+
+  function showNextLetterAndWord() {
+    if (index < letters.length) {
+      letters[index].classList.add("cp3");
+      words[index].classList.add("show-word");
+      index++;
+      setTimeout(showNextLetterAndWord, 1000); // Muda a cada 1 segundo
+    }
+  }
+
+  showNextLetterAndWord(); // Inicia a animação
+});
--- js/test.js
+++ js/test.js
@@ -7,22 +7,3 @@
 // });
 
 // hero carrousel
-
-//animacao impacto
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const letters = document.querySelectorAll(".impacto-word .letter");
-//   const words = document.querySelectorAll(".impacto-list .word");
-//   let index = 0;
-
-//   function showNextLetterAndWord() {
-//     if (index < letters.length) {
-//       letters[index].classList.add("cp3");
-//       words[index].classList.add("show-word");
-//       index++;
-//       setTimeout(showNextLetterAndWord, 1000); // Muda a cada 1 segundo
-//     }
-//   }
-
-//   showNextLetterAndWord(); // Inicia a animação
-// });
